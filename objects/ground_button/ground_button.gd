ff6adadd492be12ba75bb37bdb6c281d500cd229
Алексей Смирнов <debugger94@gmail.com>
2022-12-06 22:44:41 +0300

    StoneGate

--- objects/ground_button/ground_button.gd
+++ objects/ground_button/ground_button.gd
@@ -4,6 +4,7 @@ class_name GroundButton extends StaticBody2D
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite
 @onready var unpressed_collision: CollisionPolygon2D = $UnpressedCollision
 @onready var pressed_collision: CollisionPolygon2D = $PressedCollision
+@onready var stone_gate: StaticBody2D = $StoneGate
 
 var is_button_pressed := false
 
@@ -13,6 +14,7 @@ func press_button() -> void:
 	unpressed_collision.set_deferred("disabled", true)
 	pressed_collision.set_deferred("disabled", false)
 	animated_sprite.play("pressed")
+	stone_gate.open_stone_gate()
 
 
 func unpress_button() -> void:
@@ -20,6 +22,7 @@ func unpress_button() -> void:
 	unpressed_collision.set_deferred("disabled", false)
 	pressed_collision.set_deferred("disabled", true)
 	animated_sprite.play("unpressed")
+	stone_gate.close_stone_gate()
 
 func _on_press_detector_body_entered(body: Node2D) -> void:
 	if body is RigidBox or body is Player:
--- objects/stone_gate/stone_gate.gd
+++ objects/stone_gate/stone_gate.gd
@@ -1,9 +1,60 @@
 extends StaticBody2D
 
 
+enum States { IDLE, CLOSE, OPEN }
+
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite
+@onready var collision: CollisionShape2D = $Collision
+
+var state := States.IDLE
+var is_closed := true
+var state_queue: Array[States]
+
+
+func _process(delta: float) -> void:
+	match state:
+		States.IDLE:
+			animated_sprite.play("idle")
+		
+		States.CLOSE:
+			if not is_closed:
+				collision.disabled = false
+				animated_sprite.play("close")
+				await animated_sprite.animation_finished
+				is_closed = true
+				state = States.IDLE
+		
+		States.OPEN:
+			if is_closed:
+				animated_sprite.play("open")
+				await animated_sprite.animation_finished
+				collision.disabled = true
+				is_closed = false
+	
+	if state_queue.size() > 0:
+		var next_state := state_queue.back() as States
+		
+		if state == next_state:
+			return
+		
+		if next_state == States.CLOSE:
+			if state == States.IDLE:
+				state_queue.clear()
+				return
+		
+			if state == States.OPEN:
+				state = States.CLOSE
+				state_queue.clear()
+				return
+		
+		if next_state == States.OPEN:
+			state = States.OPEN
+			state_queue.clear()
+
+
 func close_stone_gate() -> void:
-	print("closed")
+	state_queue.append(States.CLOSE)
 
 
 func open_stone_gate() -> void:
-	print("opened")
+	state_queue.append(States.OPEN)
